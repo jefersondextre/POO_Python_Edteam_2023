c088f707598fc34ad88b14f8fa904a0411bea3a6
estadisticaDESI_ <estadisticainternacional2021@gmail.com>
2023-07-03 17:00:01 -0500

    Herencia de clases

---
+++ herencia.py
@@ -0,0 +1,41 @@
+class Vehiculo:
+  # aributos
+  def __init__(self, marca, modelo, velocidad, anio):
+    self.marca = marca,
+    self.modelo = modelo,
+    self.velocidad= velocidad,
+    self.anio = anio
+  # metodos
+  def darVelocidad(self, velocidad):
+    self.velocidad += velocidad
+
+  def reducirVelocidad(self, velocidad):
+    self.velocidad -= velocidad
+  
+class Motocicleta(Vehiculo):
+  def __init__(self, marca, modelo, velocidad, anio, motor):
+    super().__init__(marca,modelo,velocidad,anio)
+    self.motor = motor
+  
+  # Hacer caballito
+  def Wheelie(self):
+    return "Haciendo el wheelie....🐎 !!"
+  
+class Autobus(Vehiculo):
+  def __init__(self, marca, modelo, velocidad, anio, asientos):
+    super().__init__(marca, modelo, velocidad, anio)
+    self.asientos= asientos
+    
+  def cargarPasajeros(self,pasajeros):
+    return f"Pasajeros a bordo {pasajeros}"
+  
+  
+
+motocicleta = Motocicleta("Honda","2021",100,2023,1200)
+# print(motocicleta.motor,'HP')
+# print(motocicleta.marca)
+# print(motocicleta.Wheelie())
+
+autobus=Autobus("Blubird",2010,velocidad=100,anio=2023,asientos=30)
+print(autobus.modelo)
+print(autobus.cargarPasajeros())

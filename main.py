5205a965a189eb0e573e9214acb1c7ecab371d2c
jadex_Desktop <jadexb04@gmail.com>
2023-07-02 22:42:37 -0500

    Modelo de una clase basica, sin constructor

---
+++ main.py
@@ -0,0 +1,16 @@
+# Ejemplo de una clase
+
+class Usuario():
+    nombre = ""
+    apellidos = ""
+    correo = ""
+    contrasenia=""
+    telefono = ""
+    
+    
+    def encriptarContrasenia():
+      pass
+    
+    def verificarContrasenia():
+      pass
+    
